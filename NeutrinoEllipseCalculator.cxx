53236aeca0faaa8c1660ad8a30ee8c2c15d743eb
Gala <gnn4@cornell.edu>
2014-04-02 09:15:17 -0500

    updates and bug fixes

--- NeutrinoEllipseCalculator.cxx
+++ NeutrinoEllipseCalculator.cxx
@@ -8,35 +8,12 @@ NeutrinoEllipseCalculator::NeutrinoEllipseCalculator()
 NeutrinoEllipseCalculator::~NeutrinoEllipseCalculator()
 {
   //cout << "destructor" << endl;
-  delete bJet_;
-  delete lepton_;
-  delete Ab_;
-  delete Al_;
-  delete Htilde_;
-  delete H_;
-  delete Hperp_;
-  delete Nperp_;
 }
 
-NeutrinoEllipseCalculator::NeutrinoEllipseCalculator(//const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> >& bJetLorentzVector,
-						     //const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> >& leptonLorentzVector,
-						     //TLorentzVector& bJetLorentzVector, TLorentzVector& leptonLorentzVector,
-						     //math::XYZTLorentzVector& bJetLorentzVector, 
-						     double bJetPx, double bJetPy, double bJetPz, double bJetE,
-						     //math::XYZTLorentzVector& leptonLorentzVector,
+NeutrinoEllipseCalculator::NeutrinoEllipseCalculator(double bJetPx, double bJetPy, double bJetPz, double bJetE,
 						     double leptonPx, double leptonPy, double leptonPz, double leptonE,
-						     double& mTop, double& mW, double& mNu) :
-  bJet_  (new math::XYZTLorentzVector(0,0,0,0)),
-  lepton_(new math::XYZTLorentzVector(0,0,0,0)),
-  Ab_(new TMatrixD(4,4)),
-  Al_(new TMatrixD(4,4)),
-  Htilde_(new TMatrixD(3,3)),
-  H_(new TMatrixD(3,3)),
-  Hperp_(new TMatrixD(3,3)),
-  Nperp_(new TMatrixD(3,3))
+						     double mTop, double mW, double mNu) 
 {
-  //setbJet(bJetLorentzVector);
-  //setLepton(leptonLorentzVector);
   setBJet(bJetPx,bJetPy,bJetPz,bJetE);
   setLepton(leptonPx,leptonPy,leptonPz,leptonE);
 
@@ -48,8 +25,23 @@ NeutrinoEllipseCalculator::NeutrinoEllipseCalculator(//const ROOT::Math::Lorentz
   setAngles();
 
   initializeMatrices();
+}
 
-  bJetSF_=1.;
+void NeutrinoEllipseCalculator::setupEllipse(double bJetPx, double bJetPy, double bJetPz, double bJetE,
+					     double leptonPx, double leptonPy, double leptonPz, double leptonE,
+					     double mTop, double mW, double mNu) 
+{
+  setBJet(bJetPx,bJetPy,bJetPz,bJetE);
+  setLepton(leptonPx,leptonPy,leptonPz,leptonE);
+
+  setBJetRelativisticFactors();
+  setLeptonRelativisticFactors();
+
+  setMasses(mTop,mW,mNu);
+
+  setAngles();
+
+  initializeMatrices();
 }
 
 void NeutrinoEllipseCalculator::setMasses(double& mTop, double& mW, double& mNu)
@@ -63,65 +55,64 @@ void NeutrinoEllipseCalculator::setMasses(double& mTop, double& mW, double& mNu)
   mnu2_=mnu_*mnu_;
 }
 
-//void NeutrinoEllipseCalculator::setbJet(const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> >& bJetLorentzVector)
-//void NeutrinoEllipseCalculator::setbJet(const TLorentzVector& bJetLorentzVector)
-//void NeutrinoEllipseCalculator::setbJet(const math::XYZTLorentzVector& bJetLorentzVector)
 void NeutrinoEllipseCalculator::setBJet(const double px, const double py, const double pz, const double E)
 {
-  //bJet_=bJetLorentzVector;
-  bJet_->SetPxPyPzE(px,py,pz,E);
+  bJet_.SetPxPyPzE(px,py,pz,E);
   setBJetRelativisticFactors();
   setAngles();
 }
 
-//void NeutrinoEllipseCalculator::setLepton(const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> >& leptonLorentzVector)
-//void NeutrinoEllipseCalculator::setLepton(const TLorentzVector& leptonLorentzVector)
-//void NeutrinoEllipseCalculator::setLepton(const math::XYZTLorentzVector& leptonLorentzVector)
 void NeutrinoEllipseCalculator::setLepton(const double px, const double py, const double pz, const double E)
 {
-  //lepton_=leptonLorentzVector;
-  lepton_->SetPxPyPzE(px,py,pz,E);
+  lepton_.SetPxPyPzE(px,py,pz,E);
   setLeptonRelativisticFactors();
   setAngles();
 }
 
 void NeutrinoEllipseCalculator::setBJetRelativisticFactors()
 {
-  bJetBeta_=bJet_->Beta();
+  bJetBeta_=bJet_.Beta();
   bJetBeta2_=pow(bJetBeta_,2);
-  bJetGamma_=bJet_->Gamma();
+  bJetGamma_=bJet_.Gamma();
   bJetGamma2_=pow(bJetGamma_,2);
 }
 
 void NeutrinoEllipseCalculator::setLeptonRelativisticFactors()
 {
-  leptonBeta_=lepton_->Beta();
+  leptonBeta_=lepton_.Beta();
   leptonBeta2_=pow(leptonBeta_,2);
-  leptonGamma_=lepton_->Gamma();
+  leptonGamma_=lepton_.Gamma();
   leptonGamma2_=pow(leptonGamma_,2);
 }
 
 void NeutrinoEllipseCalculator::setAngles()
 {
-  c_=ROOT::Math::VectorUtil::CosTheta(*lepton_,*bJet_);
+  c_=ROOT::Math::VectorUtil::CosTheta(lepton_,bJet_);
   s_=sqrt(1.-c_*c_);
 }
 
 void NeutrinoEllipseCalculator::initializeMatrices()
 {
-  Ab_->Zero();
-  Al_->Zero();
-  Htilde_->Zero();
-  H_->Zero();
-  Hperp_->Zero();
-  Nperp_->Zero();
+  Ab_=TMatrixD(4,4);
+  Al_=TMatrixD(4,4);
+  Htilde_=TMatrixD(3,3);
+  H_=TMatrixD(3,3);
+  Hperp_=TMatrixD(3,3);
+  Nperp_=TMatrixD(3,3);
+
+  Ab_.Zero();
+  Al_.Zero();
+  Htilde_.Zero();
+  H_.Zero();
+  Hperp_.Zero();
+  Nperp_.Zero();
 }
 
 void NeutrinoEllipseCalculator::Wsurface()
 {
-  x0p_=-(0.5/  bJet_->E())*(mt2_- mW2_-bJet_  ->M2());
-  x0_ =-(0.5/lepton_->E())*(mW2_-mnu2_-lepton_->M2());
-  Sx_=(1./leptonBeta2_)*(x0_*leptonBeta_-lepton_->P()*(1.-leptonBeta2_));
+  x0p_=-(0.5/  bJet_.E())*(mt2_- mW2_-bJet_  .M2());
+  x0_ =-(0.5/lepton_.E())*(mW2_-mnu2_-lepton_.M2());
+  Sx_=(1./leptonBeta2_)*(x0_*leptonBeta_-lepton_.P()*(1.-leptonBeta2_));
   epsilon2_=(1.-leptonBeta2_)*(mW2_-mnu2_);
 }
 
@@ -180,11 +171,12 @@ void NeutrinoEllipseCalculator::neutrinoSolution()
   x1_=Sx_-(1./Omega2)*(Sx_+omega_*Sy_);
   y1_=Sy_-(1./Omega2)*omega_*(Sx_+omega_*Sy_);
   Z2_=x1_*x1_*Omega2-(Sy_-omega_*Sx_)*(Sy_-omega_*Sx_)-(mW2_-x0_*x0_-epsilon2_);
+  if( Z2_<0 ) calcBJetCorrection();
   double Z=sqrt(max(0.,Z2_));
 
   Htilde_[0][0]=Z/Omega_;
   Htilde_[0][1]=0;
-  Htilde_[0][2]=x1_-lepton_->P();
+  Htilde_[0][2]=x1_-lepton_.P();
 	
   Htilde_[1][0]=omega_*Z/Omega_;
   Htilde_[1][1]=0;
@@ -193,12 +185,6 @@ void NeutrinoEllipseCalculator::neutrinoSolution()
   Htilde_[2][0]=0;
   Htilde_[2][1]=Z;
   Htilde_[2][2]=0;
-
-  if( Z2_<0 ) 
-    {
-      //cout << "Z^2 is <0" << endl;
-      //calcbJetCorrection();
-    }
 
 }
 
@@ -229,24 +215,22 @@ void NeutrinoEllipseCalculator::labSystemTransform()
   //rotate Htilde to H
   TMatrixD R(3,3);
   R.Zero();
-  TMatrixD Rz=rotationMatrix(2,-lepton_->Phi());
-  TMatrixD Ry=rotationMatrix(1,0.5*M_PI-lepton_->Theta());
-  double bJetP[3]={bJet_->Px(),bJet_->Py(), bJet_->Pz()};
+  TMatrixD Rz=rotationMatrix(2,-lepton_.Phi());
+  TMatrixD Ry=rotationMatrix(1,0.5*M_PI-lepton_.Theta());
+  double bJetP[3]={bJet_.Px(),bJet_.Py(), bJet_.Pz()};
   TMatrixD bJet_xyz(3,1,bJetP);
   TMatrixD rM(Ry,TMatrixD::kMult,TMatrixD(Rz,TMatrixD::kMult,bJet_xyz));
   double* rA=rM.GetMatrixArray();
   double phi=-TMath::ATan2(rA[2],rA[1]);
   TMatrixD Rx=rotationMatrix(0,phi);
   R=TMatrixD(Rz,TMatrixD::kTransposeMult,TMatrixD(Ry,TMatrixD::kTransposeMult,Rx.T()));
-  //double* Ha=(TMatrixD(R,TMatrixD::kMult,*Htilde_)).GetMatrixArray();
-  //H_->SetMatrixArray(Ha);
-  (*H_)=TMatrixD(R,TMatrixD::kMult,*Htilde_);
+  H_=TMatrixD(R,TMatrixD::kMult,Htilde_);
 
 
   //calculate Hperp
-  double Hvalues[9]={(*H_)[0][0],(*H_)[0][1],(*H_)[0][2],(*H_)[1][0],(*H_)[1][1],(*H_)[1][2],0,0,1};
+  double Hvalues[9]={H_[0][0],H_[0][1],H_[0][2],H_[1][0],H_[1][1],H_[1][2],0,0,1};
   TArrayD Harray(9,Hvalues);
-  Hperp_->SetMatrixArray(Harray.GetArray());
+  Hperp_.SetMatrixArray(Harray.GetArray());
 
 
   //calculate Nperp
@@ -269,89 +253,175 @@ void NeutrinoEllipseCalculator::calcNeutrinoEllipse()
   labSystemTransform();
 }
 
-TMatrixD NeutrinoEllipseCalculator::getNeutrinoEllipse()
+TMatrixD* NeutrinoEllipseCalculator::getNeutrinoEllipse()
 {
-  calcNeutrinoEllipse();
-  TMatrixD neutrinoEllipse=*Nperp_;
-  return neutrinoEllipse;
+  //calcNeutrinoEllipse();
+  return &Nperp_;
 }
 
-TMatrixD NeutrinoEllipseCalculator::getHomogeneousNeutrinoEllipse()
+TMatrixD* NeutrinoEllipseCalculator::getHomogeneousNeutrinoEllipse()
 {
-  calcNeutrinoEllipse();
-  TMatrixD neutrinoEllipse=*Hperp_;
-  return neutrinoEllipse;
+  //calcNeutrinoEllipse();
+  return &Hperp_;
 }
-
 
 void NeutrinoEllipseCalculator::calcBJetCorrection()
 {
-  if( Z2_>0. ) return;
-
-  //If Z^2<0, solve for the b-jet energy necessary to make Z^2 exactly zero
-  //In this configuration H11=H12=H21=H22=0 and the neutrino solution is a
-  //point (H13,H23)
-
+  //Find the ranges of b-jet energies in which Z^2>=0:
   //Z^2=0 is equivalent to a polynomial of degree 4 in Eb
-  //bJetBeta_=bJetP/bJetE does not change when the energy correction is applied 
+  //bJetBeta_=bJetP/bJetE does not change when the energy correction is applied
   //(Cancels in numerator and denominator)
   //All other parameters are independent of the b-jet energy and momentum
+  //if Z^2>0 already this will simply return the interval in which the current b-jet energy is
 
-  double r0=-pow(mt2_,2) + 2.*mt2_*mW2_ - pow(mW2_,2) - pow(c_*mt2_*bJetBeta_ - c_*mW2_*bJetBeta_ - mt2_*leptonBeta_ + mW2_*leptonBeta_,2)/
+  double a0=-pow(mt2_,2) + 2.*mt2_*mW2_ - pow(mW2_,2) - pow(c_*mt2_*bJetBeta_ - c_*mW2_*bJetBeta_ - mt2_*leptonBeta_ + mW2_*leptonBeta_,2)/
     (-pow(c_*bJetBeta_ - leptonBeta_,2) + pow(s_,2)*pow(bJetBeta_,2)*(-1. + pow(leptonBeta_,2)))  ;
-  double r1=-4*mt2_*Sx_*leptonBeta_ + 4*mW2_*Sx_*leptonBeta_ - (2*(c_*mt2_*bJetBeta_ - c_*mW2_*bJetBeta_ - mt2_*leptonBeta_ + mW2_*leptonBeta_)*
+
+  double a1=-4*mt2_*Sx_*leptonBeta_ + 4*mW2_*Sx_*leptonBeta_ - (2*(c_*mt2_*bJetBeta_ - c_*mW2_*bJetBeta_ - mt2_*leptonBeta_ + mW2_*leptonBeta_)*
 								(2*c_*Sx_*bJetBeta_*leptonBeta_ - 2*Sx_*pow(leptonBeta_,2) + 2*pow(s_,2)*Sx_*pow(bJetBeta_,2)*pow(leptonBeta_,2)))/
     (-pow(c_*bJetBeta_ - leptonBeta_,2) + pow(s_,2)*pow(bJetBeta_,2)*(-1 + pow(leptonBeta_,2)))   ;
-  double r2=2.*mt2_ - 2.*mW2_ - 2.*mt2_*pow(bJetBeta_,2) + 2.*mW2_*pow(bJetBeta_,2) - 4.*pow(Sx_,2)*pow(leptonBeta_,2) + 
+
+  double a2=2.*mt2_ - 2.*mW2_ - 2.*mt2_*pow(bJetBeta_,2) + 2.*mW2_*pow(bJetBeta_,2) - 4.*pow(Sx_,2)*pow(leptonBeta_,2) + 
     (-2.*(c_*mt2_*bJetBeta_ - c_*mW2_*bJetBeta_ - mt2_*leptonBeta_ + mW2_*leptonBeta_)*(-(c_*bJetBeta_) + c_*pow(bJetBeta_,3) + leptonBeta_ - pow(bJetBeta_,2)*leptonBeta_) - 
      pow(2.*c_*Sx_*bJetBeta_*leptonBeta_ - 2.*Sx_*pow(leptonBeta_,2) + 2.*pow(s_,2)*Sx_*pow(bJetBeta_,2)*pow(leptonBeta_,2),2))/
     (-pow(c_*bJetBeta_ - leptonBeta_,2) + pow(s_,2)*pow(bJetBeta_,2)*(-1. + pow(leptonBeta_,2))) - 4.*pow(s_,2)*pow(bJetBeta_,2)*(mW2_ - pow(x0_,2) - epsilon2_)   ;
-  double r3=4.*Sx_*leptonBeta_ - 4.*Sx_*pow(bJetBeta_,2)*leptonBeta_ - (2.*(-(c_*bJetBeta_) + c_*pow(bJetBeta_,3) + leptonBeta_ - pow(bJetBeta_,2)*leptonBeta_)*
+
+  double a3=4.*Sx_*leptonBeta_ - 4.*Sx_*pow(bJetBeta_,2)*leptonBeta_ - (2.*(-(c_*bJetBeta_) + c_*pow(bJetBeta_,3) + leptonBeta_ - pow(bJetBeta_,2)*leptonBeta_)*
 									(2.*c_*Sx_*bJetBeta_*leptonBeta_ - 2.*Sx_*pow(leptonBeta_,2) + 2.*pow(s_,2)*Sx_*pow(bJetBeta_,2)*pow(leptonBeta_,2)))/
     (-pow(c_*bJetBeta_ - leptonBeta_,2) + pow(s_,2)*pow(bJetBeta_,2)*(-1. + pow(leptonBeta_,2)))   ;
-  double r4=-1. + 2.*pow(bJetBeta_,2) - pow(bJetBeta_,4) - pow(-(c_*bJetBeta_) + c_*pow(bJetBeta_,3) + leptonBeta_ - pow(bJetBeta_,2)*leptonBeta_,2)/
+
+  double a4=-1. + 2.*pow(bJetBeta_,2) - pow(bJetBeta_,4) - pow(-(c_*bJetBeta_) + c_*pow(bJetBeta_,3) + leptonBeta_ - pow(bJetBeta_,2)*leptonBeta_,2)/
     (-pow(c_*bJetBeta_ - leptonBeta_,2) + pow(s_,2)*pow(bJetBeta_,2)*(-1. + pow(leptonBeta_,2)))   ;
 
   
   //Solve for the roots of the polynomial
   //Consider only the real positive roots
 
-  Polynomial* quartic = new Polynomial(r4,r3,r2,r1,r0);
-  vector<double > bJetEValues = quartic->FindRealRoots();
+  Polynomial quartic(a4,a3,a2,a1,a0);
+  vector<double > bJetEs = quartic.FindRealRoots();
 
-  for( vector<double>::iterator ibJetE = bJetEValues.begin(); ibJetE != bJetEValues.end(); ibJetE++ )
+  for( vector<double>::iterator ibJetE = bJetEs.begin(); ibJetE != bJetEs.end(); ibJetE++ )
     {
-      double bJetE=*ibJetE;
-      if( bJetE<0. ) 
+      if( *ibJetE<0 ) 
 	{
-	  cout << "This root has a negative value: " << bJetE << "/nRemoving it from the vector of roots" << endl;
-	  bJetEValues.erase(ibJetE);
+	  //cout << "This root has a negative value: " << *ibJetE << "/nRemoving it from the vector of roots" << endl;
+	  bJetEs.erase(ibJetE);
 	}
     }
 
+  //Store the interval closest to the measured jet energy where Z^2 is positive as a vector of pairs of doubles and bools: 
 
-  //Find the root closest to the measured b-jet energy
-  
-  double minDelta=1.e9;
-  double closestEnergy=-1.;
-
-  for( vector<double>::iterator ibJetE = bJetEValues.begin(); ibJetE != bJetEValues.end(); ibJetE++ )
+  //bJetLogSFRange_.clear();
+  double  lowEdge=0; bool  hasLowEdge=false;
+  double highEdge=0; bool hasHighEdge=false;
+  nRanges_=0;
+  if( bJetEs.size() > 0 ) 
     {
-      double bJetE=*ibJetE;
-      double thisDelta=abs(bJetE-bJet_->E());
-      if( thisDelta<minDelta )
+      vector<bool> isPositiveSection;
+      vector<double> edgesLogScale;
+      double e(0.);//, deltaEMin=abs(bJetEs.at(0)-bJet_.E());
+      e = 0.5*bJetEs.at(0);
+      (a0+a1*e+a2*pow(e,2)+a3*pow(e,3)+a4*pow(e,4) > 0) ? isPositiveSection.push_back(true) : isPositiveSection.push_back(false);
+      unsigned int middleBin = 0;
+      for( unsigned int i=0; i<bJetEs.size(); i++ )
 	{
-	  minDelta=thisDelta;
-	  closestEnergy=bJetE;
+	  ( i == bJetEs.size()-1 ) ? e = 2.*bJetEs.at(bJetEs.size()-1) : e = 0.5*(bJetEs.at(i)+bJetEs.at(i+1));
+	  if(a0+a1*e+a2*pow(e,2)+a3*pow(e,3)+a4*pow(e,4) > 0)
+	    {
+	      if(*(isPositiveSection.rbegin())) continue;
+	      isPositiveSection.push_back(true);
+	    }
+	  else
+	    {
+	      if(!*(isPositiveSection.rbegin())) continue;
+	      isPositiveSection.push_back(false);
+	    }
+	  double edge = log(bJetEs.at(i)/bJet_.E());
+	  edgesLogScale.push_back(edge);
+	  if(edge < 0 ) middleBin++;
 	}
+      if(isPositiveSection[middleBin])
+	{
+	  if(middleBin != isPositiveSection.size()-1)
+	    {
+	      hasHighEdge=true;
+	      highEdge=edgesLogScale[middleBin];
+	    }
+	  if(middleBin != 0)
+	    {
+	      hasLowEdge=true;
+	      lowEdge=edgesLogScale[middleBin-1];
+	    }
+	}
+      else if(isPositiveSection.size() > 1) 
+	{
+	  if(middleBin == 0)
+	    {
+	      if(2 != isPositiveSection.size())
+		{
+		  hasHighEdge=true;
+		  highEdge=edgesLogScale[1];
+		}
+	      hasLowEdge=true;
+	      lowEdge=edgesLogScale[0];
+	    }
+	  else if(middleBin == isPositiveSection.size() - 1)
+	    {
+	      hasHighEdge=true;
+	      highEdge=edgesLogScale[middleBin-1];
+	      if(middleBin-1 != 0)
+		{
+		  hasLowEdge=true;
+		  lowEdge=edgesLogScale[middleBin-2];
+		}
+	    }
+	  else if(abs(edgesLogScale[middleBin-1])>edgesLogScale[middleBin])
+	    {
+	      middleBin-=1;
+	      if(middleBin != isPositiveSection.size()-1)
+		{
+		  hasHighEdge=true;
+		  highEdge=edgesLogScale[middleBin];
+		}
+	      if(middleBin != 0)
+		{
+		  hasLowEdge=true;
+		  lowEdge=edgesLogScale[middleBin-1];
+		}
+	    }
+	  else
+	    {
+	      middleBin+=1;
+	      if(middleBin != isPositiveSection.size()-1)
+		{
+		  hasHighEdge=true;
+		  highEdge=edgesLogScale[middleBin];
+		}
+	      if(middleBin != 0)
+		{
+		  hasLowEdge=true;
+		  lowEdge=edgesLogScale[middleBin-1];
+		}
+	    }
+	}
+      for(vector<bool>::iterator thisRange = isPositiveSection.begin(); thisRange != isPositiveSection.end(); thisRange++)
+	{
+	  if(*thisRange) nRanges_++;
+	}
+      
     }
 
+  bJetLogSFRange_.first =  make_pair( lowEdge, hasLowEdge) ;
+  bJetLogSFRange_.second = make_pair(highEdge,hasHighEdge) ;
 
-  //Energy scale factor
-  if( closestEnergy>0 ) bJetSF_=closestEnergy/bJet_->E();
-
+  return;
 }
 
+pair<pair<double,bool>, pair<double,bool> > NeutrinoEllipseCalculator::getBJetLogSFRange(int& nRanges)
+{
+  nRanges = nRanges_;
+  return bJetLogSFRange_;
+}
 
 void NeutrinoEllipseCalculator::print3By3Matrix(const TMatrixD& m)
 {
